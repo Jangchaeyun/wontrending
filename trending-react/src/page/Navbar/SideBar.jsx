32f25aa0fb9ec1941c027458421579c20dbf0d0a
Jangchaeyun <zzangsally@naver.com>
2024-10-05 21:03:02 +0900

    1005 Navbar

---
+++ trending-react/src/page/Navbar/SideBar.jsx
@@ -0,0 +1,78 @@
+import { Button } from "@/components/ui/button";
+import { SheetClose } from "@/components/ui/sheet";
+import {
+  ActivityLogIcon,
+  BookmarkIcon,
+  DashboardIcon,
+  ExitIcon,
+  HomeIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
+import { CreditCardIcon, LandmarkIcon, WalletIcon } from "lucide-react";
+import React from "react";
+
+const menu = [
+  { name: "홈", path: "/", icon: <HomeIcon className="h-6 w-6" /> },
+  {
+    name: "포트폴리오",
+    path: "/porrfolio",
+    icon: <DashboardIcon className="h-6 w-6" />,
+  },
+  {
+    name: "관심목록",
+    path: "/watchlist",
+    icon: <BookmarkIcon className="h-6 w-6" />,
+  },
+  {
+    name: "활동",
+    path: "/activity",
+    icon: <ActivityLogIcon className="h-6 w-6" />,
+  },
+  {
+    name: "지갑",
+    path: "/wallet",
+    icon: <WalletIcon className="h-6 w-6" />,
+  },
+  {
+    name: "결제 세부 정보",
+    path: "/payment-details",
+    icon: <LandmarkIcon className="h-6 w-6" />,
+  },
+  {
+    name: "인출",
+    path: "/withdrawal",
+    icon: <CreditCardIcon className="h-6 w-6" />,
+  },
+  {
+    name: "프로필",
+    path: "/profile",
+    icon: <PersonIcon className="h-6 w-6" />,
+  },
+  {
+    name: "로그아웃",
+    path: "/",
+    icon: <ExitIcon className="h-6 w-6" />,
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className="mt-10 space-y-5">
+      {menu.map((item) => (
+        <div key={item.name}>
+          <SheetClose className="w-full">
+            <Button
+              variant="outline"
+              className="flex items-center gap-5 py-6 w-full"
+            >
+              <span className="w-8">{item.icon}</span>
+              <p>{item.name}</p>
+            </Button>
+          </SheetClose>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;
