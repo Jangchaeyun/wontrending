58d0e70aedb96c1c1032d3208a7b2d816e9fb962
Jangchaeyun <zzangsally@naver.com>
2024-10-06 20:14:09 +0900

    1006 HomePage

--- trending-react/src/page/Home/AssetTable.jsx
+++ trending-react/src/page/Home/AssetTable.jsx
@@ -14,7 +14,6 @@ import React from "react";
 const AssetTable = () => {
   return (
     <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
       <TableHeader>
         <TableRow>
           <TableHead className="w-[100px]">코인</TableHead>
@@ -26,21 +25,23 @@ const AssetTable = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {[1, 1, 1, 1, 1, 1].map((item, index) => (
-          <TableRow key={index}>
-            <TableCell className="font-medium flex items-center gap-2">
-              <Avatar className="-z-50">
-                <AvatarImage src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png" />
-              </Avatar>
-              <span>Bitcoin</span>
-            </TableCell>
-            <TableCell>BTC</TableCell>
-            <TableCell>6834455792</TableCell>
-            <TableCell>376962336317</TableCell>
-            <TableCell>-19129.99</TableCell>
-            <TableCell className="text-right">19071.68₩</TableCell>
-          </TableRow>
-        ))}
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(
+          (item, index) => (
+            <TableRow key={index}>
+              <TableCell className="font-medium flex items-center gap-2">
+                <Avatar className="-z-50">
+                  <AvatarImage src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png" />
+                </Avatar>
+                <span>Bitcoin</span>
+              </TableCell>
+              <TableCell>BTC</TableCell>
+              <TableCell>6834455792</TableCell>
+              <TableCell>376962336317</TableCell>
+              <TableCell>-19129.99</TableCell>
+              <TableCell className="text-right">19071.68₩</TableCell>
+            </TableRow>
+          )
+        )}
       </TableBody>
     </Table>
   );
--- trending-react/src/page/Home/Home.jsx
+++ trending-react/src/page/Home/Home.jsx
@@ -1,11 +1,30 @@
 import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 import AssetTable from "./AssetTable";
+import StockChart from "./StockChart";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Cross1Icon, DotIcon } from "@radix-ui/react-icons";
+import { CrossIcon, MessageCircle } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 const Home = () => {
   const [category, setCategory] = useState("all");
+  const [inputValue, setInputValue] = useState("");
+  const [isBotRealease, setIsBotRealease] = useState(false);
+
+  const handleBotRealease = () => setIsBotRealease(!isBotRealease);
+
   const handleCategory = (value) => {
     setCategory(value);
+  };
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleKeyPress = (event) => {
+    if (event.key == "Enter") {
+      console.log(inputValue);
+    }
+    setInputValue("");
   };
   return (
     <div className="relative">
@@ -43,7 +62,93 @@ const Home = () => {
           </div>
           <AssetTable />
         </div>
+        <div className="hidden lg:block lg:w-[50%] p-5">
+          <StockChart />
+          <div className="flex gap-5 items-center">
+            <div>
+              <Avatar>
+                <AvatarImage
+                  src={
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png"
+                  }
+                />
+              </Avatar>
+            </div>
+            <div className="flex items-center gap-2">
+              <p>ETH</p>
+              <DotIcon className="text-gray-400" />
+              <p className="text-gray-400">Ethereum</p>
+            </div>
+            <div className="flex items-end gap-2">
+              <p className="text-xl font-bold">5464</p>
+              <p className="text-red-600">
+                <span>-19129.99</span>
+                <span>(-0.29803%)</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
+      <section className="absolute bottom-5 right-5 z-40 flex flex-col justify-end items-end gap-2">
+        {isBotRealease && (
+          <div className="rounded-md w-[20rem] md:w-[25rem] lg:w-[25rem] h-[70vh] bg-slate-50">
+            <div className="flex justify-between items-center border-b px-6 h-[12%]">
+              <p>챗 봇</p>
+              <Button onClick={handleBotRealease} variant="ghost" size="icon">
+                <Cross1Icon />
+              </Button>
+            </div>
+            <div className="h-[76%] flex flex-col overflow-y-auto gap-5 px-5 py-2 scroll-container">
+              <div className="self-start pb-5 w-auto">
+                <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-100  w-auto">
+                  <p>안녕하세요!</p>
+                  <p>투자 관련 질문은 무엇이든 할 수 있습니다</p>
+                  <p>예를 들어, 가격, 시가총액 등...</p>
+                </div>
+              </div>
+              {[1, 1, 1, 1].map((item, i) => (
+                <div
+                  key={i}
+                  className={`${
+                    i % 2 == 0 ? "self-start" : "self-end"
+                  } "pb-5 w-auto"`}
+                >
+                  {i % 2 == 0 ? (
+                    <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-100  w-auto">
+                      <p>당신은 누구입니까?</p>
+                    </div>
+                  ) : (
+                    <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-100  w-auto">
+                      <p>ans, hi, Band Aid</p>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="h-[12%] border-t">
+              <Input
+                className="w-full h-full order-none outline-none"
+                placeholder="프롬프트를 작성하세요"
+                onChange={handleChange}
+                value={inputValue}
+                onKeyPress={handleKeyPress}
+              />
+            </div>
+          </div>
+        )}
+        <div className="relative w-[10rem] cursor-pointer group">
+          <Button
+            onClick={handleBotRealease}
+            className="w-full h-[3rem] gap-2 items-center"
+          >
+            <MessageCircle
+              size={30}
+              className="fill-[#ffffff] -rotate-90 stroke-none group-hover:fill[#1a1a1a"
+            />
+            <span className="text-2xl">챗 봇</span>
+          </Button>
+        </div>
+      </section>
     </div>
   );
 };
