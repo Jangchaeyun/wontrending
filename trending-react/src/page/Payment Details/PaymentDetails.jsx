22a855d28de7e7e2f1de4cf556eb16d361cae9c0
Jangchaeyun <zzangsally@naver.com>
2024-10-08 17:36:20 +0900

    1008 PaymentDetail

--- trending-react/src/page/Payment Details/PaymentDetails.jsx
+++ trending-react/src/page/Payment Details/PaymentDetails.jsx
@@ -1,7 +1,57 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import React from "react";
+import PaymentDetailsForm from "./PaymentDetailsForm";
 
 const PaymentDetails = () => {
-  return <div>PaymentDetails</div>;
+  return (
+    <div className="px-20">
+      <h1 className="text-3xl font-bold py-10">결제 세부정보</h1>
+      {true ? (
+        <Card>
+          <CardHeader>
+            <CardTitle>토스 뱅크</CardTitle>
+            <CardDescription>주식 소유 계좌 : **********1651</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center">
+              <p className="w-32">주식 소유자</p>
+              <p className="text-gray-400"> : Band Aid</p>
+            </div>
+            <div className="flex items-center">
+              <p className="w-32">SWIFT CODE</p>
+              <p className="text-gray-400"> : HVBKKRSEXXX</p>
+            </div>
+          </CardContent>
+        </Card>
+      ) : (
+        <Dialog>
+          <DialogTrigger>
+            <Button className="py-6">결제 세부정보 추가</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>결제 세부정보</DialogTitle>
+            </DialogHeader>
+            <PaymentDetailsForm />
+          </DialogContent>
+        </Dialog>
+      )}
+    </div>
+  );
 };
 
 export default PaymentDetails;
---
+++ trending-react/src/page/Payment Details/PaymentDetailsForm.jsx
@@ -0,0 +1,134 @@
+import { Button } from "@/components/ui/button";
+import { DialogClose } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const PaymentDetailsForm = () => {
+  const form = useForm({
+    resolver: "",
+    defaultValues: {
+      accountHolderName: "",
+      bic: "",
+      accountNumber: "",
+      confirmAccountNumber: "",
+      bankName: "",
+    },
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="px-10 py-2">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="accountHolderName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>주식 소유자 이름</FormLabel>
+                <FormControl>
+                  <Input
+                    name="accountHolderName"
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="홍길동"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="bic"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>BIC CODE</FormLabel>
+                <FormControl>
+                  <Input
+                    name="bic"
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="HVBKKRSEXXX"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="accountNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>계좌 번호</FormLabel>
+                <FormControl>
+                  <Input
+                    name="accountNumber"
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="***********3456"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmAccountNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>계좌 번호 확인</FormLabel>
+                <FormControl>
+                  <Input
+                    name="confirmAccountNumber"
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="***********3456"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="bankName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>은행 이름</FormLabel>
+                <FormControl>
+                  <Input
+                    name="bankName"
+                    className="border w-full border-gray-700 p-5"
+                    placeholder="토스 뱅크"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <DialogClose className="w-full">
+            <Button type="submit" className="w-full py-5">
+              저장
+            </Button>
+          </DialogClose>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default PaymentDetailsForm;
