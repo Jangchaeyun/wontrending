f46886ad15fad4ce161ad68dbbff3df654b920c5
Jangchaeyun <zzangsally@naver.com>
2024-10-08 20:15:58 +0900

    1008 Profile

--- trending-react/src/page/Profile/Profile.jsx
+++ trending-react/src/page/Profile/Profile.jsx
@@ -1,7 +1,104 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { VerifiedIcon } from "lucide-react";
 import React from "react";
+import AccountVartificationForm from "./AccountVartificationForm";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const handleEnableTwoStepVerficiation = () => {
+    console.log("two step verfication");
+  };
+  return (
+    <div className="flex flex-col items-center mb-5">
+      <div className="pt-10 w-full lg:w-[60%]">
+        <Card>
+          <CardHeader className="pb-9">나의 정보</CardHeader>
+          <CardContent>
+            <div className="lg:flex gap-32">
+              <div className="space-y-7">
+                <div className="flex">
+                  <p className="w-[9rem]">이메일: </p>
+                  <p className="text-gray-500">bandaid@gmail.com</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[9rem]">이름: </p>
+                  <p className="text-gray-500">장채윤</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[9rem]">생일: </p>
+                  <p className="text-gray-500">2000/11/12</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[9rem]">국적: </p>
+                  <p className="text-gray-500">한국</p>
+                </div>
+              </div>
+              <div className="space-y-7">
+                <div className="flex">
+                  <p className="w-[9rem]">이메일: </p>
+                  <p className="text-gray-500">bandaid@gmail.com</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[9rem]">이름: </p>
+                  <p className="text-gray-500">장채윤</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[9rem]">생일: </p>
+                  <p className="text-gray-500">2000/11/12</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[9rem]">국적: </p>
+                  <p className="text-gray-500">한국</p>
+                </div>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+        <div className="mt-6">
+          <Card className="w-full">
+            <CardHeader className="pb-7">
+              <div className="flex items-center gap-3">
+                <CardTitle>2단계 인증</CardTitle>
+                {true ? (
+                  <Badge className={"space-x-2 text-white bg-green-600"}>
+                    <VerifiedIcon />
+                    <span>인증 완료</span>
+                  </Badge>
+                ) : (
+                  <Badge className="bg-orange-500">인증 안됨</Badge>
+                )}
+              </div>
+            </CardHeader>
+            <CardContent>
+              <div>
+                <Dialog>
+                  <DialogTrigger>
+                    <Button>2단계 인증 활성화</Button>
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>계정을 확인하세요</DialogTitle>
+                    </DialogHeader>
+                    <AccountVartificationForm
+                      handleSubmit={handleEnableTwoStepVerficiation}
+                    />
+                  </DialogContent>
+                </Dialog>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
